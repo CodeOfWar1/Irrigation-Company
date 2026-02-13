da067ed801bce963b7b54a38d4725df15869bfe8
Mike-tembo <tembomike375@gamil.com>
2026-02-13 12:33:25 +0200

    changes the cta.js background and contenr

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,41 +1,51 @@
 import React from 'react';
-import kws from '../images/clients/kws.png';
-import geps from '../images/clients/geps.png';
-import protergia from '../images/clients/protergia.png';
-
-const clientImage = {
-    height: '10rem',
-    width: 'auto',
-    mixBlendMode: 'colorBurn'
-}
+import kws from '../images/clients/isl.jpg';
+import geps from '../images/clients/unza.png';
+import protergia from '../images/clients/Zicta.png';
 
 const Clients = () => {
     return (
         <div className="mt-8 bg-gray-100">
             <section data-aos="fade-up">
                 <div className="my-4 py-4">
-                        <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">Our Clients</h2>
-                        <div className='flex justify-center'>
-                            <div className='w-24 border-b-4 border-blue-900'></div>
-                        </div>
-                        <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-blue-900">Some of our clients.</h2>
+                    <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">
+                        Our Clients
+                    </h2>
+                    <div className="flex justify-center">
+                        <div className="w-24 border-b-4 border-blue-900"></div>
                     </div>
+                    <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-blue-900">
+                        Some of our clients.
+                    </h2>
+                </div>
 
                 <div className="p-16" data-aos="fade-in" data-aos-delay="600">
-                    <div className="grid sm:grid-cols-3 lg:grid-cols-3">
-                        <div style={clientImage} className= "overflow-hidden flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100 w-1/6">
-                            <img src={kws} alt="client" />                           
+                    <div className="grid grid-cols-1 sm:grid-cols-3 gap-10 place-items-center">
+
+                        <div className="flex items-center justify-center h-40 w-full">
+                            <img
+                                src={kws}
+                                alt="ISL"
+                                className="h-32 w-auto object-contain"
+                            />
                         </div>
 
-                        <div style={clientImage} className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100">
-                            <img src={protergia} alt="client" />                            
-                        </div> 
-
-                        <div style={clientImage} className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100">
-                            <img src={geps} alt="client" />                            
+                        <div className="flex items-center justify-center h-40 w-full">
+                            <img
+                                src={protergia}
+                                alt="ZICTA"
+                                className="h-32 w-auto object-contain"
+                            />
                         </div>
 
-                                           
+                        <div className="flex items-center justify-center h-40 w-full">
+                            <img
+                                src={geps}
+                                alt="UNZA"
+                                className="h-32 w-auto object-contain"
+                            />
+                        </div>
+
                     </div>
                 </div>
             </section>
